1b8c354ad2411bad9ff9bfbdd8d7eaa8294f7f02
Ares <ares_5210@163.com>
2018-11-22 02:55:39 +0000

    Add account activations

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -27,8 +27,14 @@ class SessionController extends Controller
 		]);
 
 		if(Auth::attempt($user, $request->has('remember'))) {
-			session()->flash('success', '欢迎回来！');
-			return redirect()->intended(route('users.show', Auth::user()->id));
+            if(Auth::user()->activated) {
+                session()->flash('success', '欢迎回来！');
+                return redirect()->intended(route('users.show', Auth::user()->id));
+            } else {
+                Auth::logout();
+                session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+                return redirect('/');
+            }
 		} else {
 			session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
 			return redirect()->back()->withInput();
